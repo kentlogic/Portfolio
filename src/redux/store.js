66d14c7d52c76f7cdf13a90fe185be6d5baa22c9
kentlogic <iict.vincent@gmail.com>
2020-08-23 18:56:32 +0800

    Removed unused packages in package.json | Updated the store.js - add thunkMiddleware to list of middlewares on prod build.

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,10 @@ import rootReducer from "./root-reducer";
 const middlewares = [];
 
 if (process.env.NODE_ENV === "development") {
-  middlewares.push(logger, thunkMiddleware);
+  middlewares.push(logger);
 }
+
+middlewares.push(thunkMiddleware);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
